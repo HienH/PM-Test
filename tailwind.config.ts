2637040d74835aaf6a561cb09cc7cb011ae505bd
hienh <hienhang@hotmail.co.uk>
2025-12-04 20:30:48 +0300

    chore: initalize project with tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Add brand colors
+        brand: "#4F46E5",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
